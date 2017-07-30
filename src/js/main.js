503332c84dd52d9a771e2bb5bcbedca9315f04f8
Vinesh Kannan <vinesh@omnipointment.com>
2017-07-30 00:13:43 -0500

    Set up initial project structure

---
+++ src/js/main.js
@@ -0,0 +1,44 @@
+console.log('Welcome to ScarletHacks.');
+
+let signupEmail = document.getElementById('signup-email');
+let signupButton = document.getElementById('signup-submit');
+
+let postSignupEmail = (email) => {
+	return new Promise((resolve, reject) => {
+		console.log(`Email: ${email}`);
+		if (email) {
+			if (email.indexOf('@') > -1) {
+				setTimeout(() => {
+					resolve();
+				}, 1000);
+			} else {
+				reject('Please enter a valid email address.');
+			}
+		} else {
+			reject('Please enter a valid email address.');
+		}
+	});
+}
+
+let signupWithEmail = () => {
+	signupButton.classList.add('is-loading');
+	let email = signupEmail.value;
+	postSignupEmail(email).then((done) => {
+		signupButton.classList.remove('is-loading');
+		vex.dialog.alert(`We'll let you know when ScarletHacks applications are out!`);
+	}).catch((err) => {
+		signupButton.classList.remove('is-loading');
+		vex.dialog.alert(err);
+	});
+}
+
+signupEmail.addEventListener('keypress', (e) => {
+	let keyCode = e.keyCode || e.which;
+	if (keyCode == 13) {
+		signupWithEmail();
+	}
+});
+
+signupButton.addEventListener('click', (e) => {
+	signupWithEmail();
+});
