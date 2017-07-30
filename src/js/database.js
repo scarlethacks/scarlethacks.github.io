c0569813708d515bd8fbbdb639418537453ce07e
Vinesh Kannan <vinesh@omnipointment.com>
2017-07-30 17:43:02 -0500

    Anonymous Firebase authentication

--- src/js/database.js
+++ src/js/database.js
@@ -1,12 +1,38 @@
 let Database = (firebase, config) => {
 
 	let ScarletHacksFirebase = firebase.initializeApp(config, 'ScarletHacks Database');
+	config.localhost = false;
 	let prometheus = Prometheus(config);
-	
+	let db = ScarletHacksFirebase.database();
+
 	let database = {
+
+		init: () => {
+			return new Promise((resolve, reject) => {
+				firebase.auth().signInAnonymously().catch((err) => {
+					console.error(err.code, err.message);
+					reject(err);
+				});
+				firebase.auth().onAuthStateChanged((user) => {
+					if (user) {
+						prometheus.logon(user.uid);
+						resolve(user);
+					} else {
+						console.log('User is signed out.');
+					}
+				});
+			});
+		},
 
 		getPrometheus: () => {
 			return prometheus;
+		},
+
+		saveSignupEmail: (email) => {
+			return db.ref('email-signups').push({
+				email: email,
+				timestamp: Date.now()
+			});
 		}
 
 	}
--- src/js/main.js
+++ src/js/main.js
@@ -7,49 +7,59 @@ let prometheusjs = require('prometheusjs');
 
 let database = Database(firebase, config);
 
-console.log(config);
-console.log(database);
-console.log('Welcome to ScarletHacks.');
+database.init().then((user) => {
+	main(user);
+}).catch((err) => {
+	vex.dialog.alert(err);
+});
 
-let signupEmail = document.getElementById('signup-email');
-let signupButton = document.getElementById('signup-submit');
+let main = (user) => {
 
-let postSignupEmail = (email) => {
-	return new Promise((resolve, reject) => {
-		console.log(`Email: ${email}`);
-		if (email) {
-			if (email.indexOf('@') > -1) {
-				setTimeout(() => {
-					resolve();
-				}, 1000);
+	console.log('Welcome to ScarletHacks.');
+	console.log(user);
+
+	let prometheus = database.getPrometheus();
+
+	let signupEmail = document.getElementById('signup-email');
+	let signupButton = document.getElementById('signup-submit');
+
+	let postSignupEmail = (email) => {
+		return new Promise((resolve, reject) => {
+			console.log(`Email: ${email}`);
+			if (email) {
+				if (email.indexOf('@') > -1) {
+					database.saveSignupEmail(email).then(resolve).catch(reject);
+				} else {
+					reject('Please enter a valid email address.');
+				}
 			} else {
 				reject('Please enter a valid email address.');
 			}
-		} else {
-			reject('Please enter a valid email address.');
+		});
+	}
+
+	let signupWithEmail = () => {
+		signupButton.classList.add('is-loading');
+		let email = signupEmail.value;
+		postSignupEmail(email).then((done) => {
+			signupButton.classList.remove('is-loading');
+			vex.dialog.alert(`We'll let you know when ScarletHacks applications are out!`);
+		}).catch((err) => {
+			signupButton.classList.remove('is-loading');
+			let errMessage = `Something went wrong: ${err}`;
+			vex.dialog.alert(errMessage);
+		});
+	}
+
+	signupEmail.addEventListener('keypress', (e) => {
+		let keyCode = e.keyCode || e.which;
+		if (keyCode == 13) {
+			signupWithEmail();
 		}
 	});
-}
 
-let signupWithEmail = () => {
-	signupButton.classList.add('is-loading');
-	let email = signupEmail.value;
-	postSignupEmail(email).then((done) => {
-		signupButton.classList.remove('is-loading');
-		vex.dialog.alert(`We'll let you know when ScarletHacks applications are out!`);
-	}).catch((err) => {
-		signupButton.classList.remove('is-loading');
-		vex.dialog.alert(err);
-	});
-}
-
-signupEmail.addEventListener('keypress', (e) => {
-	let keyCode = e.keyCode || e.which;
-	if (keyCode == 13) {
+	signupButton.addEventListener('click', (e) => {
 		signupWithEmail();
-	}
-});
+	});
 
-signupButton.addEventListener('click', (e) => {
-	signupWithEmail();
-});
+}
